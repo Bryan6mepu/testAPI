da660d9779f51fabdf76150a551ef8af0b2d24b9
sullivan-mandeville <sullivan.mandeville@condorcet.be>
2021-02-03 15:12:50 +0100

    PremierCtrler

---
+++ API/API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/Home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            String test = "test";
+            return Ok(test);
+        }
+    }
+}
